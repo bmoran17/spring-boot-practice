8a83bffd648a1c91a36cc6c841cf04fa5c84ac24
Brenda Moran <110500766+bmoran17@users.noreply.github.com>
2026-02-17 21:08:18 -0800

    Added REST controller to handle /

---
+++ src/main/java/com/example/my_app/FunRestController.java
@@ -0,0 +1,13 @@
+package com.example.my_app;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class FunRestController {
+  
+  // expose "/" that returns "Hello World"
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello World!";
+  }
+}
